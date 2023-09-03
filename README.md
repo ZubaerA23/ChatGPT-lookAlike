7e04a2520786ff12b2129d4112514a37a97ba093
ZubaerA23 <105396721+ZubaerA23@users.noreply.github.com>
2023-09-03 18:51:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ChatGPT-lookAlike
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-lt3vnt)
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+import  {useEffect , useState} from 'react';
+
+
+function App() {
+ return(
+   <div className = "main">
+     <div className = "left">
+       <p>ZubaerGPT<span>•</span></p>
+       <h1 data-text = "Welcome to ZubaerGPT"></h1>
+       
+       
+       </div>
+      <div className = "right">
+          <h1>Get Started</h1>
+          <div className = "mini">
+          <button className ="button_one"> Sign up </button>
+          <button className = "button_two"> Log in </button>
+          </div>
+        </div>
+   </div>
+ )
+}
+
+export default App;
